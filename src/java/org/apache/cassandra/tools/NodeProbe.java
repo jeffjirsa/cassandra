89480c05e6e1045d023460ca42df19537fe4838e
Jeff Jirsa <jeff@jeffjirsa.net>
2015-11-14 21:33:53 -0800

    CASSANDRA-10660: Support user-defined compactions through nodetool

--- src/java/org/apache/cassandra/tools/NodeProbe.java
+++ src/java/org/apache/cassandra/tools/NodeProbe.java
@@ -287,6 +287,10 @@ public class NodeProbe implements AutoCloseable
         }
     }
 
+    public void forceUserDefinedCompaction(String datafiles) throws IOException, ExecutionException, InterruptedException
+    {
+        compactionProxy.forceUserDefinedCompaction(datafiles);
+    }
 
     public void forceKeyspaceCompaction(boolean splitOutput, String keyspaceName, String... columnFamilies) throws IOException, ExecutionException, InterruptedException
     {
