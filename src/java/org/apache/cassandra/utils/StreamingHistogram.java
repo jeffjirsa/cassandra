5d50c365d7dbdcfbd2b27999d336c037f295800f
Jeff Jirsa <jeff@jeffjirsa.net>
2017-03-01 14:23:54 -0800

    Remove spool size from hash code

--- src/java/org/apache/cassandra/utils/StreamingHistogram.java
+++ src/java/org/apache/cassandra/utils/StreamingHistogram.java
@@ -304,7 +304,7 @@ public class StreamingHistogram
     @Override
     public int hashCode()
     {
-        return Objects.hashCode(bin.hashCode(), spool.hashCode(), maxBinSize, maxSpoolSize);
+        return Objects.hashCode(bin.hashCode(), spool.hashCode(), maxBinSize);
     }
 
 }
